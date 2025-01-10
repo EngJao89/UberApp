62a0829eee3a89a0adf2db674b65d4ddda748f3d
EngJao89 <eng_joao@outlook.com>
2025-01-09 23:00:08 -0300

    Create race status.

---
+++ UberApp/StatusCorrida.swift
@@ -0,0 +1,9 @@
+//
+//  StatusCorrida.swift
+//  UberApp
+//
+//  Created by João Ricardo Martins Ribeiro on 09/01/25.
+//
+
+case EmRequisicao, PegarPassageiro, IniciarViagem, EmViagem, ViagemFinalizada
+}
